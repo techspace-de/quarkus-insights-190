f15ff7857d3e56e738ae163f573232d5f12edfed
Christian Berger <chberger@users.noreply.github.com>
2024-12-16 00:41:55 +0100

    feat: basic integration tests

---
+++ src/test/java/de/techspace/quarkus/code/stage/ExtensionResourceIT.java
@@ -0,0 +1,7 @@
+package de.techspace.quarkus.code.stage;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExtensionResourceIT extends ExtensionResourceTest {
+}
